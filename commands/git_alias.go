2e39a6f360241a2acb76a55ce688bbd7547e79db
Poliyka <poliyka.hsu@gmail.com>
2024-06-05 20:04:19 +0800

    fix: git alias command bug

--- commands/git_alias.go
+++ commands/git_alias.go
@@ -21,27 +21,29 @@ func GitAlias() {
 	fi
 	`
 
-	GITCONFIG_PATH = fmt.Sprintf(installCmd, GITCONFIG_PATH)
+	installCmd = fmt.Sprintf(installCmd, GITCONFIG_PATH)
 
-	insertStr := `
+	insertStr := fmt.Sprintf(`cat <<EOF > %s
 st = status
 cm = commit
 ch = checkout
 sw = switch
 br = branch
 mg = merge
-acm =  "!git add -A && git commit -m"
+acm = "!git add -A && git commit -m"
 mgd = "!git mg $1 && git br -d $1; #"
 # 查看分支(樹狀圖)
-lg = log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
+lg = log --color --graph --pretty=format:'%%Cred%%h%%Creset -%%C(yellow)%%d%%Creset %%s %%Cgreen(%%cr) %%C(bold blue)<%%an>%%Creset' --abbrev-commit
 # 查看reflog(HeighLight)
-rl = reflog --pretty=format:\"%Cred%h%Creset %C(auto)%gd%Creset %C(auto)%gs%C(reset) %C(green)(%cr)%C(reset) %C(bold blue)<%an>%Creset\" --abbrev-commit
+rl = reflog --pretty=format:\"%%Cred%%h%%Creset %%C(auto)%%gd%%Creset %%C(auto)%%gs%%C(reset) %%C(green)(%%cr)%%C(reset) %%C(bold blue)<%%an>%%Creset\" --abbrev-commit
 # 查看stash(HeighLight)
-sl = stash list --pretty=format:\"%C(red)%h%C(reset) - %C(dim yellow)(%C(bold magenta)%gd%C(dim yellow))%C(reset) %<(70,trunc)%s %C(green)(%cr) %C(bold blue)<%an>%C(reset)\"
-`
+sl = stash list --pretty=format:\"%%C(red)%%h%%(reset) - %%C(dim yellow)(%%C(bold magenta)%%gd%%C(dim yellow))%%(reset) %%(70,trunc)%%s %%C(green)(%%cr) %%C(bold blue)<%%an>%%(reset)\"
+EOF
+`, GITCONFIG_PATH)
 
-	commands := []string{installCmd}
-	commands = append(commands, fmt.Sprintf(`echo '%s' >> %s`, insertStr, GITCONFIG_PATH))
+
+	commands := []string{installCmd, insertStr}
+	fmt.Println(commands)
 
 	core.ExecuteCommands(commands)
 }

fa5dca0cbbe6ada79d2da75a9e3f0cc7a0d2967b
Poliyka <poliyka.hsu@gmail.com>
2024-06-05 19:28:48 +0800

    fix: bash color wrong indent

--- commands/bash_color.go
+++ commands/bash_color.go
@@ -9,12 +9,12 @@ func BashColor() {
 	fmt.Println(core.StdGreen("Installing BashColor"))
 
 	installCmd := `sed -i -e '$a\
-	\
-	parse_git_branch() {\
-	  git branch 2> /dev/null | sed -e \"/^[^*]/d\" -e \"s/* \\(.*\\)/ (\\1)/\"\
-	}\
-	export PS1=\"\\[\\033[01;32m\\]\\u@\\h\\[\\e[91m\\]\\$(parse_git_branch) \\[\\e[1;33m\\]\\D{%Y/%m/%d} \\t\\[\\033[00m\\]:\\n\\[\\e[34m\\]\\w\\[\\e[00m\\]\$ \"
-	' $HOME/.bashrc`
+\
+parse_git_branch() {\
+	git branch 2> /dev/null | sed -e \"/^[^*]/d\" -e \"s/* \\(.*\\)/ (\\1)/\"\
+}\
+export PS1=\"\\[\\033[01;32m\\]\\u@\\h\\[\\e[91m\\]\\$(parse_git_branch) \\[\\e[1;33m\\]\\D{%Y/%m/%d} \\t\\[\\033[00m\\]:\\n\\[\\e[34m\\]\\w\\[\\e[00m\\]\$ \"
+' $HOME/.bashrc`
 
 	commands := []string{installCmd}
 

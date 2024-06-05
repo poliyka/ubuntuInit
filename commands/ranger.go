4e1a49bdc0683d9fa8c6124dfd157a648da1f4dd
Poliyka <poliyka.hsu@gmail.com>
2024-06-05 15:57:41 +0800

    fix: qoute error

--- commands/ranger.go
+++ commands/ranger.go
@@ -9,8 +9,8 @@ func Ranger() {
 	fmt.Println(core.StdGreen("Installing Ranger"))
 	commands := []string{
 		"sudo git clone https://github.com/ranger/ranger.git $HOME/ranger",
-		"sudo su - -c \"cd ${HOME}/ranger; make install\"",
-		"sudo su $USER -c \"ranger --copy-config=all\"",
+		`sudo su - -c "cd ${HOME}/ranger; make install"`,
+		`sudo su $USER -c "ranger --copy-config=all"`,
 		"sudo rm -rf $HOME/ranger",
 				`sed -i -e '$a\
 \nclass code(Command):\

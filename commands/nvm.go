6e66925cb0852c0aeb61e2a8febe86ee720e0b0e
Poliyka <poliyka.hsu@gmail.com>
2024-06-05 21:42:42 +0800

    fix: remove nvm print verions

--- commands/nvm.go
+++ commands/nvm.go
@@ -8,7 +8,6 @@ import (
 func Nvm() {
 	fmt.Println(core.StdGreen("Installing Nvm"))
 	version := core.GetGithubRepLatestRelease("nvm-sh", "nvm")
-	fmt.Println(core.StdGreen(version))
 
 	commands := []string{
 		fmt.Sprintf(`sudo su $USER -c "curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/%s/install.sh" | /bin/bash`, version),

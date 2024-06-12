05c98e659437815e6d0cc4744e19bfa47bf33b4a
Poliyka <poliyka.hsu@gmail.com>
2024-06-12 16:58:55 +0800

    fix: Docker install process auto yes

--- commands/docker.go
+++ commands/docker.go
@@ -35,7 +35,7 @@ sudo apt-get update
 		// Add Gpg key && repository
 		GPGCmd,
 		// Install Docker
-		"sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin",
+		"sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin",
 	}
 
 	core.ExecuteCommands(commands)
